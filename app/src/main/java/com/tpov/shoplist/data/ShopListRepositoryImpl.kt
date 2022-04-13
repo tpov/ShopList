220b338335e7862073afb2f2c7063578018af20e
Oleg <33009369+tpov@users.noreply.github.com>
2022-04-13 13:20:48 +0300

    lesson3(data)

---
+++ app/src/main/java/com/tpov/shoplist/data/ShopListRepositoryImpl.kt
@@ -0,0 +1,36 @@
+package com.tpov.shoplist.data
+
+import com.tpov.shoplist.domain.ShopItem
+import com.tpov.shoplist.domain.ShopListRepository
+
+object ShopListRepositoryImpl: ShopListRepository {
+    private val shopList = mutableListOf<ShopItem>()
+
+    private var autoIncrementId = 0
+
+    override fun addShopItem(shopItem: ShopItem) {
+        if (shopItem.id == ShopItem.UNDEFINED_ID) shopItem.id = autoIncrementId++
+        shopList.add(shopItem)
+    }
+
+    override fun deleteShopItem(shopItem: ShopItem) {
+        shopList.remove(shopItem)
+    }
+
+    override fun editShopItem(shopItem: ShopItem) {
+        val oldItem = getShopItem(shopItem.id)
+        shopList.remove(oldItem)
+        shopList.add(shopItem)
+    }
+
+    override fun getShopItem(shopItemId: Int): ShopItem {
+        return shopList.find {
+            it.id == shopItemId
+        } ?: throw RuntimeException("Element with id $shopItemId not found")
+    }
+
+    override fun getShopList(): List<ShopItem> {
+        return shopList.toList()
+    }
+
+}
