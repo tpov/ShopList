7c0ec33a565fcde7174c877b58b86cefeb166fa6
Oleg <33009369+tpov@users.noreply.github.com>
2022-04-15 14:00:59 +0300

    lesson7??

--- app/src/main/java/com/tpov/shoplist/presentation/MainActivity.kt
+++ app/src/main/java/com/tpov/shoplist/presentation/MainActivity.kt
@@ -2,11 +2,18 @@ package com.tpov.shoplist.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
 import com.tpov.shoplist.R
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var viewModel: MainViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        viewModel = ViewModelProvider(this)[MainViewModel::class.java]
+        viewModel.shopList.
     }
+
+
 }
