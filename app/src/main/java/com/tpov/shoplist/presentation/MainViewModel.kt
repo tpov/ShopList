d76792f72b31d56fef4bf97165dea9333eaea74a
Oleg <33009369+tpov@users.noreply.github.com>
2022-04-15 18:49:28 +0300

    lesson8

--- app/src/main/java/com/tpov/shoplist/presentation/MainViewModel.kt
+++ app/src/main/java/com/tpov/shoplist/presentation/MainViewModel.kt
@@ -1,26 +1,28 @@
-package com.tpov.shoplist.presentation
+package com.sumin.shoppinglist.presentation
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.tpov.shoplist.data.ShopListRepositoryImpl
+import com.sumin.shoppinglist.data.ShopListRepositoryImpl
 import com.tpov.shoplist.domain.DeleteShopItemUseCase
 import com.tpov.shoplist.domain.EditShopItemUseCase
 import com.tpov.shoplist.domain.GetShopListUseCase
 import com.tpov.shoplist.domain.ShopItem
 
-class MainViewModel: ViewModel() {
+class MainViewModel : ViewModel() {
 
     private val repository = ShopListRepositoryImpl
 
     private val getShopListUseCase = GetShopListUseCase(repository)
-    private val editShopItemUseCase = EditShopItemUseCase(repository)
     private val deleteShopItemUseCase = DeleteShopItemUseCase(repository)
+    private val editShopItemUseCase = EditShopItemUseCase(repository)
 
     val shopList = getShopListUseCase.getShopList()
 
     fun deleteShopItem(shopItem: ShopItem) {
         deleteShopItemUseCase.deleteShopItem(shopItem)
     }
+
     fun changeEnableState(shopItem: ShopItem) {
         val newItem = shopItem.copy(enabled = !shopItem.enabled)
         editShopItemUseCase.editShopItemUseCase(newItem)
